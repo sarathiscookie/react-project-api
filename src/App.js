ad6b24c9ea630709b80594e1d9b399a6c186b39d
Sarath TS <sarathiscookie@hotmail.com>
2021-03-11 21:36:36 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h4>Welcome</h4>
+      </div>
+    )
+  }
+}
+
+export default App;
