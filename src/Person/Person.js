4879887706e2202a58302c69404579dec3f7cf47
Sarath TS <sarathiscookie@hotmail.com>
2021-03-11 21:42:34 +0530

    Components: Function component.

---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userDetails = () => {
+    return (
+        <div>
+            <h5>I am Peter.</h5>
+            <h6>I am from unknown world.</h6>
+        </div>
+    )
+}
+
+export default userDetails;
